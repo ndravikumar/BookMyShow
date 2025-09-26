d29e78941414b1675ca342a76db8d230aa7da538
RaviKumar <naramalaravi@gmail.com>
2025-09-27 00:11:19 +0530

    url change

--- Frontend/src/api/index.js
+++ Frontend/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5173/bms",
+  baseURL: `${import.meta.env.VITE_API_URL}`,
   headers: {
     "Content-Type": "application/json",
   },
