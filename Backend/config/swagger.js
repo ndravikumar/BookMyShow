c3d430155746096441af315d8a17d645b2e98a27
RaviKumar <naramalaravi@gmail.com>
2025-10-07 11:52:24 +0530

    swagger second commit

--- Backend/config/swagger.js
+++ Backend/config/swagger.js
@@ -1,20 +1,9 @@
 const swaggerJSDoc = require("swagger-jsdoc");
 const swaggerUi = require("swagger-ui-express");
+const swaggerDef = require("./swagger.json");
 
 const options = {
-  definition: {
-    openapi: "3.0.0",
-    info: {
-      title: "BookMyShow API",
-      version: "1.0.0",
-      description: "API documentation for BookMyShow backend",
-    },
-    servers: [
-      {
-        url: "http://localhost:5000", // change if needed
-      },
-    ],
-  },
+  definition: swaggerDef,
   apis: ["./routes/*.js"], // path to your route files
 };
 
--- Backend/routes/movieRoute.js
+++ Backend/routes/movieRoute.js
@@ -9,16 +9,6 @@ const { validateJWTToken } = require("../middleware/authorizationMiddleware");
 const router = require("express").Router();
 
 router.get("/movie/:id", getMovieById);
-/**
- * @swagger
- * /api/movies:
- *   get:
- *     summary: Get all movies
- *     tags: [Movies]
- *     responses:
- *       200:
- *         description: List of movies
- */
 router.get("/getAllMovies", getAllMovies);
 router.post("/addMovie", addMovie);
 router.patch("/updateMovie", updateMovie);
